e9bf1f6dca87c67c481d56cd58dc8edc3930b4f1
yoheikikuta <diracdiego@gmail.com>
2023-07-16 22:47:55 +0900

    Identify tobioka's paper

--- inspirehep_request.py
+++ inspirehep_request.py
@@ -32,29 +32,41 @@ def fetch_inspirehep_papers(url):
     else:
         raise Exception(f"Failed to fetch papers: {response.status_code}")
     
-testurl=create_query_url("de%20%3E%202020%20and%20refersto%3Aauthor%3AK.Tobioka.1")
-print(testurl)
+# testurl=create_query_url("de%20%3E%202020%20and%20refersto%3Aauthor%3AK.Tobioka.1")
+# print(testurl)
 
-test_result=fetch_inspirehep_papers(testurl)
-json_dict = json.loads(test_result)
+# test_result=fetch_inspirehep_papers(testurl)
+# json_dict = json.loads(test_result)
 
-# with open('test.json') as f:
-#     data = json.load(f)
-#json_dict = data 
+# references = json_dict["hits"]["hits"]
+# n_paper=len(references)
+# print("number of citations are "+str(n_paper) +" in the last 7 days")
 
+########
+# Use local file for test
+with open('test.json') as f:
+    data = json.load(f)
+
+json_dict = data 
 references = json_dict["hits"]["hits"]
-n_paper=len(references)
-print("number of citations are "+str(n_paper) +" in the last 7 days")
+########
 
-for reference in references:
-    title = reference['metadata']['titles'][0]['title']
-    id = reference['id']
-    print("")
-    print("Title: "+title)
-    print("Link: https://inspirehep.net/literature/"+id)
-    author_full_names = [author['full_name'] for author in reference['metadata']['authors']]
-    print(author_full_names)
+# 飛岡の論文を引用している論文を調べるコード
+# for reference in references:
+#     title = reference['metadata']['titles'][0]['title']
+#     id = reference['id']
+#     print("")
+#     print("Title: "+title)
+#     print("Link: https://inspirehep.net/literature/"+id)
+#     author_full_names = [author['full_name'] for author in reference['metadata']['authors']]
+#     print(author_full_names)
 
+# 実際に引用されている飛岡の論文がどれかを調べるコード
+for ref in references[1]["metadata"]["references"]:
+    # Some reference info does not include "authors" key
+    if ref["reference"].get("authors"):
+        if "Tobioka, K." in [author["full_name"] for author in ref["reference"]["authors"]]:
+            print(ref["raw_refs"][0]["value"])
 
 #To do for output: Extract json output in a favored way, e.g. title of the citing paper, cite papers of mine
 #To do for query: (1) limit by updated dates (e.g. last week, month); exclude big papers from my paper list. 
