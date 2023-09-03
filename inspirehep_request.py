437bbe93e4a533475662c5b9ff0481bd563df764
Kohsaku-Tobioka <kohsaku.t@gmail.com>
2023-09-03 09:20:44 -0400

    refactoring done

--- inspirehep_request.py
+++ inspirehep_request.py
@@ -1,104 +1,91 @@
 import datetime
-import requests
 import json
+from urllib.parse import urlencode
 
-#https://inspirehep.net/authors/1198400?ui-citation-summary=true&ui-exclude-self-citations=true
-def create_query_url(Author_Identifier, max_results=10, page=1, sort_order='mostrecent', pastdays=7):
+import requests
+
+
+
+def create_query_url(author_identifier, max_results=10, page=1, sort_order='mostrecent', past_days=7):
+    day_start = (datetime.date.today() - datetime.timedelta(days=past_days)).strftime("%Y-%m-%d")
+    params = {
+        'q': f'de > {day_start} and refersto:author:{author_identifier}',
+        'size': max_results,
+        'page': page,
+        'sort': sort_order
+    }
     base_url = 'https://inspirehep.net/api/literature?'
-    refersto = f"refersto%3Aauthor%3A{Author_Identifier}"
-    max_results = f'size={max_results}'
-    page = f'page={page}'
-    sort_order = f'sort={sort_order}'
-    day_start=(datetime.date.today() - datetime.timedelta(days=pastdays)).strftime("%Y-%m-%d")
-    dates=f'q=de%20>%20{day_start}'
-    url = f'{base_url}{dates}%20and%20{refersto}&{max_results}&{page}&{sort_order}'
-    return url
+    return f"{base_url}{urlencode(params)}"
+
 
 def fetch_inspirehep_papers(url):
     response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        raise Exception(f"Failed to fetch papers: {response.status_code}")
+    response.raise_for_status()
+    return response.text
 
 
+def extract_author_names_from_reference(reference):
+    return [author["full_name"] for author in reference["reference"]["authors"]]
 
-def readout_citedpaper(reference,Name):
-    output_myref =[]
+
+def readout_cited_paper(reference, name):
+    output_myref = []
     for ref in reference["metadata"]["references"]:
         if ref["reference"].get("authors"):
-            if Name in [author["full_name"] for author in ref["reference"]["authors"]]:
-                if ref["reference"].get('title'): 
-                    output_myref.append("["+ref["reference"]["label"]+"]")
+            if name in extract_author_names_from_reference(ref):
+                if ref["reference"].get('title'):
+                    output_myref.append(f"[{ref['reference']['label']}]")
                     output_myref.append(ref["reference"]["title"]["title"])
                 elif ref.get("raw_refs"):
                     output_myref.append(ref["raw_refs"][0]["value"])
                 else:
-                    output_myref.append("reference-title or raw_refs not found")
-                
+                    output_myref.append("Reference title or raw_refs not found")
 
-                
     return output_myref
 
 
-def process_references(references,Auther_Name):
-    n_paper=len(references)
-    print("Number of citations are "+str(n_paper) +" in the last 7 days")
+def process_references(references, author_name):
+    print(f"Number of citations are {len(references)} in the last 7 days")
 
-    i=1
-    for reference in references:
+    for idx, reference in enumerate(references, start=1):
         title = reference['metadata']['titles'][0]['title']
-        id = reference['id']
-        print("\nRef"+str(i))
-        print("Title: "+title)
-        print("Link: https://inspirehep.net/literature/"+id)
-        if reference['metadata'].get('authors'):
-            author_full_names = [author['full_name'] for author in reference['metadata']['authors']] #fix next time
-            print("Authors: ", author_full_names)
-        elif reference['metadata'].get('corporate_author'):
-            print("Corporate_author: ", reference['metadata']['corporate_author'])
+        id_ = reference['id']
+        print(f"\nRef{idx}")
+        print(f"Title: {title}")
+        print(f"Link: https://inspirehep.net/literature/{id_}")
+        
+        authors = reference['metadata'].get('authors')
+        corporate_author = reference['metadata'].get('corporate_author')
+
+        if authors:
+            author_full_names = [author['full_name'] for author in authors]
+            print(f"Authors: {author_full_names}")
+        elif corporate_author:
+            print(f"Corporate_author: {corporate_author}")
         else:
-            print('keys: authors/corporate_author not found')
+            print('Keys: authors/corporate_author not found')
 
-        cited_refs = readout_citedpaper(reference,Auther_Name)
-        
-        if cited_refs ==[]:
-            print("Cited ref: ", Auther_Name+" not found. Possibly a big collaboration paper. ")
-        else: 
-            print("Cited ref: ", cited_refs)
-        
-        i += 1
+        cited_refs = readout_cited_paper(reference, author_name)
 
-
-
+        if not cited_refs:
+            print(f"Cited ref: {author_name} not found. Possibly a big collaboration paper.")
+        else:
+            print(f"Cited ref: {cited_refs}")
 
 
 def main():
-    a_id="K.Tobioka.1"
-    a_name="Tobioka, K."
-    url= create_query_url(a_id, pastdays=70, max_results=100)
+    author_id = "K.Tobioka.1"
+    author_name = "Tobioka, K."
+    url = create_query_url(author_id, past_days=70, max_results=100)
     print(url)
     result = fetch_inspirehep_papers(url)
     json_dict = json.loads(result)
     references = json_dict["hits"]["hits"]
-    
-    # Save json_dict to a file
-    #with open('output.json', 'w') as json_file:
-    #    json.dump(json_dict, json_file, indent=4)
-    ########
-    # Use local file for test
-    #with open('test_2023-08-13.json') as f:
-    #    data = json.load(f)
-    #json_dict = data 
-    #references = json_dict["hits"]["hits"]
-    ########
-
-    process_references(references,a_name)
+    process_references(references, author_name)
 
 
 if __name__ == "__main__":
     main()
 
-
-#to do
-# de%20>%... query order matters? Clean up this part nex time. 
+#for improvements
+# Nice to unify two inputs to one. e.g. generate Tobioka, K. from K.Tobioka.1  
